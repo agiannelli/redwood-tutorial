d054969f480f27a199118bd51d186646e9fd9443
Amanda Giannelli <agiannelli23@gmail.com>
2021-02-26 22:46:38 -0500

    Upgrade to 0.26.2

---
+++ web/src/App.js
@@ -0,0 +1,28 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import netlifyIdentity from 'netlify-identity-widget'
+
+import { FatalErrorBoundary } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+import { isBrowser } from '@redwoodjs/prerender/browserUtils'
+
+import FatalErrorPage from 'src/pages/FatalErrorPage'
+import Routes from 'src/Routes'
+
+import './scaffold.css'
+import './index.css'
+
+isBrowser && netlifyIdentity.init()
+
+const App = () => {
+  return (
+    <FatalErrorBoundary page={FatalErrorPage}>
+      <AuthProvider client={netlifyIdentity} type="netlify">
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </AuthProvider>
+    </FatalErrorBoundary>
+  )
+}
+
+export default App
